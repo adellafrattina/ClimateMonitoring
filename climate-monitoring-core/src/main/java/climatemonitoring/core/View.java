d4c7f60739bea455c386f4c2ad09f3465feed190
francescolops <flops@studenti.uninsubria.it>
2024-08-14 11:05:05 +0200

    implemented ViewState and setState in View

--- climate-monitoring-core/src/main/java/climatemonitoring/core/View.java
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/View.java
@@ -1,6 +1,12 @@
 package climatemonitoring.core;
 
-public class View {
+public class View{
+
+	private ViewState m_State; 
+
+	public void setState(ViewState state){
+		m_State = state;
+	}
 
 	
 }
--- climate-monitoring-core/src/main/java/climatemonitoring/core/ViewState.java
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/ViewState.java
@@ -2,5 +2,13 @@ package climatemonitoring.core;
 
 public abstract class ViewState {
 
+	public void onHeadlessRender(String args){
+
+	}
+
+	public void onGUIRender(){
+		
+	}
+
 	
 }
