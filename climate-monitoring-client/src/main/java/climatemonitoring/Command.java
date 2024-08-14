7aea1a0b9970fa01552ebdbcf9a563e86414c34c
francescolops <flops@studenti.uninsubria.it>
2024-08-14 13:12:41 +0200

    Fixed other errors

--- climate-monitoring-client/src/main/java/climatemonitoring/Command.java
+++ climate-monitoring-client/src/main/java/climatemonitoring/Command.java
@@ -18,6 +18,41 @@ import java.util.StringTokenizer;
  * @version 1.0-SNAPSHOT
  */
 class Command {
+	
+	/**
+	 * The command used to search areas by name, country and coords
+	 */
+	public static final String SEARCH = "search";
+	
+	/**
+	 * The command used to visualize the info about an area or a center
+	 */
+	public static final String VIEW = "view";
+	
+	/**
+	 * The command used to log into the application with user_id and password
+	 */
+	public static final String LOGIN = "login";
+	
+	/**
+	 * The command used to create a new account with user_id, surname, name, ssid, email and password
+	 */
+	public static final String REGISTER = "register";
+	
+	/**
+	 * The command to add an area, center or parameter
+	 */
+	public static final String ADD = "add";
+	
+	/**
+	 * The command to edit the operator's account
+	 */
+	public static final String EDIT = "edit";
+	
+	/**
+	 * The command to change a bunch of settings such as the application's theme to classic, light and dark
+	 */
+	public static final String SETTINGS = "settings";
 
 	/**
 	 * Parses a command line string to extract a command and its associated arguments, storing them in m_cmd and m_args
@@ -28,7 +63,7 @@ class Command {
 		StringTokenizer st = new StringTokenizer(line);
 
 		if(st.hasMoreTokens())
-			m_cmd = st.nextToken().substring(1);
+			m_cmd = st.nextToken().trim().substring(1);
 
 		if(st.hasMoreTokens())
 			m_args = st.nextToken("").trim();
@@ -52,41 +87,6 @@ class Command {
 
 		return m_args;
 	}
-	
-	/**
-	 * The command used to search areas by name, country and coords
-	 */
-	public static final String SEARCH = "search";
-
-	/**
-	 * The command used to visualize the info about an area or a center
-	 */
-
-	public static final String VIEW = "view";
-	/**
-	 * The command used to log into the application with user_id and password
-	 */
-	public static final String LOGIN = "login";
-
-	/**
-	 * The command used to create a new account with user_id, surname, name, ssid, email and password
-	 */
-	public static final String REGISTER = "register";
-
-	/**
-	 * The command to add an area, center or parameter
-	 */
-	public static final String ADD = "add";
-
-	/**
-	 * The command to edit the operator's account
-	 */
-	public static final String EDIT = "edit";
-
-	/**
-	 * The command to change a bunch of settings such as the application's theme to classic, light and dark
-	 */
-	public static final String SETTINGS = "settings";
 
 	private String m_cmd = "";
 	private String m_args = "";
