c9f93d1721cee795044227c5c2c110bdbad66b44
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-18 15:20:44 +0200

    Updated build files for Windows

--- scripts/build/windows/build-client.bat
+++ scripts/build/windows/build-client.bat
@@ -1,4 +1,6 @@
 @echo off
 cd ..\..\..
+del /f /q bin\clientCM.jar
+del /f /q bin\coreCM.jar
 call scripts\maven\mvnw.cmd -am --projects climate-monitoring-client clean install
 pause
--- scripts/build/windows/build-server.bat
+++ scripts/build/windows/build-server.bat
@@ -1,4 +1,6 @@
 @echo off
 cd ..\..\..
+del /f /q bin\serverCM.jar
+del /f /q bin\coreCM.jar
 call scripts\maven\mvnw.cmd -am --projects climate-monitoring-server clean install
 pause
