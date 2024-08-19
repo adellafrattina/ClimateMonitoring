cff9489e37cc60023ffee5825296b1012771a631
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-19 15:33:50 +0200

    Implemented ErrorType enum

---
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/ErrorType.java
@@ -0,0 +1,34 @@
+package climatemonitoring.core;
+
+/**
+ * The identifiers the database can use to assign an error message to the parameter that violated an integrity constraint or server check
+ * 
+ * @author adellafrattina
+ * @version 1.0-SNAPSHOT
+ */
+public enum ErrorType {
+
+	GEONAME_ID,
+	NAME,
+	ASCII_NAME,
+	COUNTRY_CODE,
+	COUNTRY_NAME,
+	LATITUDE,
+	LONGITUDE,
+	CENTER_ID,
+	STREET,
+	POSTAL_CODE,
+	CITY,
+	DISTRICT,
+	USER_ID,
+	SSID,
+	SURNAME,
+	EMAIL,
+	PASSWORD,
+	CATEGORY_ID,
+	DATE,
+	TIME,
+	SCORE,
+	NOTES,
+	EXPLANATION
+}
