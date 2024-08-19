fbaf90d7bbd7235b3402430d0a2c7ce89d3314e0
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-20 01:58:39 +0200

    Implemented Master view class

--- climate-monitoring-client/src/main/java/climatemonitoring/Master.java
+++ climate-monitoring-client/src/main/java/climatemonitoring/Master.java
@@ -1,13 +1,59 @@
 package climatemonitoring;
 
+import climatemonitoring.core.Area;
+import climatemonitoring.core.ConnectionLostException;
+import climatemonitoring.core.DatabaseRequestException;
+import climatemonitoring.core.ErrorType;
 import climatemonitoring.core.ViewState;
+import climatemonitoring.core.headless.Console;
 
 class Master extends ViewState {
 
 	@Override
 	public void onHeadlessRender(String args) {
 
-		throw new UnsupportedOperationException("Unimplemented method 'onHeadlessRender'");
+		Command c = new Command(args);
+
+		try {
+
+			Area[] foundAreas = null;
+			switch (c.getCmd()) {
+
+				case "name":
+					foundAreas = Handler.getProxyServer().searchAreasByName(c.getArgs());
+					break;
+				case "country":
+					foundAreas = Handler.getProxyServer().searchAreasByCountry(c.getArgs());
+					break;
+				case "coords":
+					String[] coords = c.getArgs().split(" ");
+					double latitude = Double.parseDouble(coords[0]);
+					double longitude = Double.parseDouble(coords[1]);
+					foundAreas = Handler.getProxyServer().searchAreasByCoords(latitude, longitude);
+					break;
+				default:
+					Console.write("Incorrent command syntax -->'" + c.getCmd() + "', expected [name, country, coords]");
+					return;
+			}
+
+			for (Area area : foundAreas)
+				Console.write(area.getGeonameID() + " - " + area.getName() + "(" + area.getAsciiName() + "), " + area.getCountryCode());
+		}
+
+		catch (NumberFormatException e) {
+
+			Console.write("The <latitude, longitude> field must be a couple of floating point numbers");
+		}
+
+		catch (DatabaseRequestException e) {
+
+			Console.write(e.getErrorMsg(ErrorType.GEONAME_ID));
+		}
+
+		catch (ConnectionLostException e) {
+
+			e.printStackTrace();
+		}
 	}
 
 	@Override
