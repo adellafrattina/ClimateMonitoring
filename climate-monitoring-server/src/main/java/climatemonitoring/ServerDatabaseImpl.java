86957e9106a524295911d52580a942bae71c9a27
ccapiferri <ccapiferri@studenti.uninsubria.it>
2024-08-20 14:50:51 +0200

    Removed unused import

--- climate-monitoring-server/src/main/java/climatemonitoring/ServerDatabaseImpl.java
+++ climate-monitoring-server/src/main/java/climatemonitoring/ServerDatabaseImpl.java
@@ -11,7 +11,6 @@ package climatemonitoring;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
