05628d95671fa2f1a6ca0c4b289e64e4268e29b9
ccapiferri <ccapiferri@studenti.uninsubria.it>
2024-08-24 15:46:14 +0200

    Added DatabaseMT methods and respective RequestType enums

--- climate-monitoring-core/src/main/java/climatemonitoring/core/RequestType.java
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/RequestType.java
@@ -34,6 +34,21 @@ public enum RequestType {
 	 */
 	SEARCH_AREAS_BY_COORDS,
 
+	/**
+	 * To search centers by name
+	 */
+	SEARCH_CENTERS_BY_NAME,
+
+
+	/**
+	 * To get an area based on its geoname id
+	 */
+	GET_AREA,
+
+	/**
+	 * To get a center based on its center id
+	 */
+	GET_CENTER,
 
 	/**
 	 * To get the parameters based on an area and a center
