1b8994b497555dcb33093dc9a175bd7b6fab0262
ccapiferri <ccapiferri@studenti.uninsubria.it>
2024-08-24 16:47:46 +0200

    Reformatted disconnection procedure

--- climate-monitoring-server/src/main/java/climatemonitoring/Skeleton.java
+++ climate-monitoring-server/src/main/java/climatemonitoring/Skeleton.java
@@ -294,6 +294,7 @@ class Skeleton extends Thread {
 					case DISCONNECT:
 
 						m_running = false;
+						m_out.writeObject(request);
 
 						break;
 
@@ -307,7 +308,7 @@ class Skeleton extends Thread {
 
 			catch (IOException | ConnectionLostException e) {
 
-				Console.write("Client disconnected unexpectedly");
+				Console.write("Client [" + m_client.getInetAddress() + ":" + m_client.getPort() + "] has disconnected unexpectedly");
 				m_running = false;
 			}
 
