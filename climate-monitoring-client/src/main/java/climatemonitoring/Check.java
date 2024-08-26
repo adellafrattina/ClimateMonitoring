203667d2d937e8cfa814a9550b0fc7363f71b452
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-26 17:53:45 +0200

    Changed visibility (private -> public)

--- climate-monitoring-client/src/main/java/climatemonitoring/Check.java
+++ climate-monitoring-client/src/main/java/climatemonitoring/Check.java
@@ -372,7 +372,7 @@ class Check {
 	 * @param str The desired string
 	 * @return Null if the parameter is valid, a error message as a string if the parameter is null/empty/blank
 	 */
-	private static String isEmpty(String str) {
+	public static String isEmpty(String str) {
 
 		return str == null || str.isEmpty() || str.isBlank() ? "This box must not be left blank" : null;
 	}
@@ -382,7 +382,7 @@ class Check {
 	 * @param str The desired string
 	 * @return Null if the parameter is valid, a error message as a string if the parameter has "--"
 	 */
-	private static String noDashes(String str) {
+	public static String noDashes(String str) {
 
 		return str.contains("--") ? "Contiguous dashes are not allowed" : null;
 	}
