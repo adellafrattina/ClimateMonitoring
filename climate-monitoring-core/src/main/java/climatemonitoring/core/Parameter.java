f4d7f98514e62f8b3e98e1ad96206ed81e66fbe3
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-27 22:49:25 +0200

    Resource classes are now serializable

--- climate-monitoring-core/src/main/java/climatemonitoring/core/Parameter.java
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/Parameter.java
@@ -9,13 +9,17 @@ Dariia Sniezhko 753057 VA
 
 package climatemonitoring.core;
 
+import java.io.Serializable;
+
 /**
  * Encapsulates all the properties of a parameter
  * 
  * @author dariiasniezhkoinsubria
  * @version 1.0-SNAPSHOT
  */
-public class Parameter {
+public class Parameter implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Initializes parameter fields
