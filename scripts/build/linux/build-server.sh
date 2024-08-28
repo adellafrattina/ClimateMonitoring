6365adcda35caad4b5b1ea8380dc9cc1acead821
ccapiferri <ccapiferri@studenti.uninsubria.it>
2024-08-28 19:50:26 +0200

    Added linux scripts

---
+++ scripts/build/linux/build-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ../../..
+
+chmod +x bin
+chmod +x ./scripts/maven/mvnw
+
+rm -f bin/serverCM.jar
+rm -f bin/coreCM.jar
+
+./scripts/maven/mvnw -am --projects climate-monitoring-server clean install
---
+++ scripts/build/linux/start-client.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ../../../bin/
+
+java -jar clientCM.jar
