fcff0621ab535643100a94bcdc3dbaa2aceaaa8d
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-30 15:20:26 +0200

    Implemented SearchCenter headless view

--- climate-monitoring-client/src/main/java/climatemonitoring/SearchCenter.java
+++ climate-monitoring-client/src/main/java/climatemonitoring/SearchCenter.java
@@ -9,6 +9,12 @@ Dariia Sniezhko 753057 VA
 
 package climatemonitoring;
 
+import climatemonitoring.core.Area;
+import climatemonitoring.core.Center;
+import climatemonitoring.core.ConnectionLostException;
+import climatemonitoring.core.DatabaseRequestException;
+import climatemonitoring.core.headless.Console;
+
 /**
  * To search a center
  * 
@@ -17,13 +23,50 @@ package climatemonitoring;
  */
 class SearchCenter {
 
-	public static void onHeadlessRender(String args) {
+	public synchronized static void onHeadlessRender(final String by, final String args) throws ConnectionLostException, DatabaseRequestException {
 
-		throw new UnsupportedOperationException("Unimplemented method 'onHeadlessRender'");
+		m_foundCenters = null;
+
+		// <by>
+		switch (by) {
+
+			case "name":
+				m_foundCenters = Handler.getProxyServer().searchCentersByName(args);
+				break;
+			default:
+				Console.write("Incorrect command syntax -->'" + by + "', expected [name]");
+				return;
+		}
+
+		if (m_foundCenters != null) {
+
+			int i = 0;
+			for (Center center : m_foundCenters) {
+
+				Area area = Handler.getProxyServer().getArea(center.getCity());
+				Console.write(i++ + ". " + center.getCenterID() + " - (" + area.getGeonameID() + ") " + area.getAsciiName() + " " + center.getStreet() + ", " + center.getHouseNumber());
+			}
+		}
+
+		else
+			Console.write("No matching centers");
 	}
 
-	public static void onGUIRender() {
+	public synchronized static void onGUIRender() {
 
 		throw new UnsupportedOperationException("Unimplemented method 'onGUIRender'");
 	}
+
+	public static synchronized Center[] getFoundCenters() {
+
+		return m_foundCenters;
+	}
+
+	public static synchronized Center getSelectedCenter() {
+
+		return m_selectedCenter;
+	}
+
+	private static Center[] m_foundCenters;
+	private static Center m_selectedCenter;
 }
