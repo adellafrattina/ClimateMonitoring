691eeebbedd54a09ad4344ef2b23333a6c27f356
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-30 15:05:24 +0200

    Added SearchArea class

---
+++ climate-monitoring-client/src/main/java/climatemonitoring/SearchArea.java
@@ -0,0 +1,29 @@
+/*
+
+Alessandro della Frattina 753073 VA
+Cristian Capiferri 752918 VA
+Francesco Lops 753175 VA
+Dariia Sniezhko 753057 VA
+
+*/
+
+package climatemonitoring;
+
+/**
+ * To search an area
+ * 
+ * @author adellafrattina
+ * @verison 1.0-SNAPSHOT
+ */
+class SearchArea {
+
+	public static void onHeadlessRender(String args) {
+
+		throw new UnsupportedOperationException("Unimplemented method 'onHeadlessRender'");
+	}
+
+	public static void onGUIRender() {
+
+		throw new UnsupportedOperationException("Unimplemented method 'onGUIRender'");
+	}
+}
