d440ec43de066365fb7ff6df4b97a79a4c5e1d13
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-30 15:06:09 +0200

    Added SearchCenter class

---
+++ climate-monitoring-client/src/main/java/climatemonitoring/SearchCenter.java
@@ -0,0 +1,29 @@
+/*
+
+Alessandro della Frattina 753073 VA
+Cristian Capiferri 752918 VA
+Francesco Lops 753175 VA
+Dariia Sniezhko 753057 VA
+
+*/
+
+package climatemonitoring;
+
+/**
+ * To search a center
+ * 
+ * @author adellafrattina
+ * @verison 1.0-SNAPSHOT
+ */
+class SearchCenter {
+
+	public static void onHeadlessRender(String args) {
+
+		throw new UnsupportedOperationException("Unimplemented method 'onHeadlessRender'");
+	}
+
+	public static void onGUIRender() {
+
+		throw new UnsupportedOperationException("Unimplemented method 'onGUIRender'");
+	}
+}
