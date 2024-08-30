4017a0689bfd13685d266e78caed1fde5e5e69db
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-31 00:31:22 +0200

    Implemented Widget class

--- climate-monitoring-core/src/main/java/climatemonitoring/core/gui/Widget.java
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/gui/Widget.java
@@ -9,7 +9,192 @@ Dariia Sniezhko 753057 VA
 
 package climatemonitoring.core.gui;
 
+import imgui.ImGui;
+
+/**
+ * To group all the common properties of a widget
+ * 
+ * @author adellafrattina
+ * @version 1.0-SNAPSHOT
+ */
 public class Widget {
 
+	/**
+	 * To set the widget with the default x position
+	 */
+	public static final float DEFAULT_X = -1.0f;
 
+	/**
+	 * To set the widget with the default y position
+	 */
+	public static final float DEFAULT_Y = -1.0f;
+
+	/**
+	 * To set the widget with the default width
+	 */
+	public static final float DEFAULT_WIDTH = -1.0f;
+
+	/**
+	 * To set the widget with the default height
+	 */
+	public static final float DEFAULT_HEIGHT = -1.0f;
+
+	/**
+	 * To set the widget on the same line as the previous one
+	 */
+	public static final float SAME_LINE_Y = -3.0f;
+
+	/**
+	 * To set the widget's origin
+	 * @param x The widget's x origin
+	 * @param y The widget's y origin
+	 */
+	public void setOrigin(float x, float y) {
+
+		m_originX = x;
+		m_originY = y;
+	}
+
+	/**
+	 * To set the widget's x origin
+	 * @param x The widget's x origin
+	 */
+	public void setOriginX(float x) {
+
+		m_originX = x;
+	}
+
+	/**
+	 * To set the widget's y origin
+	 * @param y The widget's y origin
+	 */
+	public void setOriginY(float y) {
+
+		m_originY = y;
+	}
+
+	/**
+	 * To set the widget's position
+	 * @param x The widget's x position
+	 * @param y The widget's y position
+	 */
+	public void setPosition(float x, float y) {
+
+		setPositionX(x);
+		setPositionY(y);
+	}
+
+	/**
+	 * To set the widget's x position
+	 * @param x The widget's x position
+	 */
+	public void setPositionX(float x) {
+
+		m_x = x == DEFAULT_X ? ImGui.getCursorPosX() : x;
+	}
+
+	/**
+	 * To set the widget's y position
+	 * @param y The widget's y position
+	 */
+	public void setPositionY(float y) {
+
+		if (y == SAME_LINE_Y) {
+
+			ImGui.sameLine();
+			y = DEFAULT_Y;
+		}
+
+		m_y = y == DEFAULT_Y ? ImGui.getCursorPosY() : y;
+	}
+
+	/**
+	 * To set the widget's size
+	 * @param width The widget's width
+	 * @param height The widget's height
+	 */
+	public void setSize(float width, float height) {
+
+		m_width = width;
+		m_height = height;
+	}
+
+	/**
+	 * To set the widget's width
+	 * @param width The widget's width
+	 */
+	public void setWidth(float width) {
+
+		m_width = width;
+	}
+
+	/**
+	 * To set the widget's height
+	 * @param height The widget's height
+	 */
+	public void setHeight(float height) {
+
+		m_height = height;
+	}
+
+	/**
+	 * 
+	 * @return The widget's x origin
+	 */
+	public float getOriginX() {
+
+		return m_originX;
+	}
+
+	/**
+	 * 
+	 * @return The widget's y origin
+	 */
+	public float getOriginY() {
+
+		return m_originY;
+	}
+
+	/**
+	 * 
+	 * @return The widget's x position
+	 */
+	public float getPositionX() {
+
+		return m_x;
+	}
+
+	/**
+	 * 
+	 * @return The widget's y position
+	 */
+	public float getPositionY() {
+
+		return m_y;
+	}
+
+	/**
+	 * 
+	 * @return The widget's width
+	 */
+	public float getWidth() {
+
+		return m_width;
+	}
+
+	/**
+	 * 
+	 * @return The widget's height
+	 */
+	public float getHeight() {
+
+		return m_height;
+	}
+
+	private float m_originX = 0.0f;
+	private float m_originY = 0.0f;
+	private float m_x = DEFAULT_X;
+	private float m_y = DEFAULT_Y;
+	private float m_width = DEFAULT_WIDTH;
+	private float m_height = DEFAULT_HEIGHT;
 }
