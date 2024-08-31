bf461cea7e1a2098a69141de63b35e0e829f2c2d
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-31 16:12:46 +0200

    Implemented DropDown class

--- climate-monitoring-core/src/main/java/climatemonitoring/core/gui/DropDown.java
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/gui/DropDown.java
@@ -9,7 +9,69 @@ Dariia Sniezhko 753057 VA
 
 package climatemonitoring.core.gui;
 
-public class DropDown {
+import imgui.ImGui;
+import imgui.ImVec2;
+import imgui.flag.ImGuiComboFlags;
+import imgui.flag.ImGuiSelectableFlags;
 
+/**
+ * To render a drop-down menu
+ * 
+ * @author adellfrattina
+ * @version 1.0-SNAPSHOT
+ */
+public class DropDown extends Widget {
 
+	/**
+	 * To create a drop-down menu
+	 * @param label The drop down label
+	 * @param selectables The selectables list
+	 */
+	public DropDown(String label, String[] selectables) {
+
+		m_label = label;
+		m_selectables = selectables;
+	}
+
+	/**
+	 * To render the drop-down menu
+	 * @return The current selected item's index
+	 */
+	public int render() {
+
+		ImGui.text(m_label);
+		ImVec2 size = ImGui.calcTextSize(m_label);
+		setPositionY(SAME_LINE_Y);
+		ImGui.setCursorPos(size.x + getPositionX(), getPositionY());
+		ImGui.pushItemWidth(getWidth());
+		if (ImGui.beginCombo("##" + m_label, m_selectables[m_currentItem], ImGuiComboFlags.HeightRegular)) {
+
+			for (int i = 0; i < m_selectables.length; i++) {
+
+				final boolean isSelected = (m_currentItem == i);
+
+				if (ImGui.selectable(m_selectables[i], isSelected, ImGuiSelectableFlags.AllowDoubleClick))
+					m_currentItem = i;
+			}
+
+			ImGui.endCombo();
+		}
+
+		ImGui.popItemWidth();
+
+		return m_currentItem;
+	}
+
+	/**
+	 * 
+	 * @return The drop-down's selectable list
+	 */
+	public String[] getList() {
+
+		return m_selectables;
+	}
+
+	private String m_label;
+	private final String[] m_selectables;
+	private int m_currentItem = 0;
 }
