d2eba1e2cb3a705339abe6bd9ac4db1c617a654e
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-31 16:28:38 +0200

    Added InputTextMultiline file

---
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/gui/InputTextMultiline.java
@@ -0,0 +1,15 @@
+/*
+
+Alessandro della Frattina 753073 VA
+Cristian Capiferri 752918 VA
+Francesco Lops 753175 VA
+Dariia Sniezhko 753057 VA
+
+*/
+
+package climatemonitoring.core.gui;
+
+public class InputTextMultiline extends Widget {
+
+
+}
