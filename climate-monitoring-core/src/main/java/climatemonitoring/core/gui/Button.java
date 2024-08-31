dd25201a7f7c2db785e7aa4b045987ae2b275d0f
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-31 13:25:29 +0200

    Implemented Button class

--- climate-monitoring-core/src/main/java/climatemonitoring/core/gui/Button.java
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/gui/Button.java
@@ -9,7 +9,43 @@ Dariia Sniezhko 753057 VA
 
 package climatemonitoring.core.gui;
 
-public class Button {
+import imgui.ImGui;
 
+/**
+ * To render a button
+ * 
+ * @author adellfrattina
+ * @version 1.0-SNAPSHOT
+ */
+public class Button extends Widget {
 
+	/**
+	 * Create the button
+	 * @param label The button label
+	 */
+	public Button(String label) {
+
+		m_label = label;
+	}
+
+	/**
+	 * To render the button
+	 * @return True if the button gets pressed, false if not
+	 */
+	public boolean render() {
+
+		return m_texture != 0 ? ImGui.imageButton(m_texture, getWidth(), getHeight()) : ImGui.button(m_label);
+	}
+
+	/**
+	 * To set a texture renderer ID
+	 * @param texture
+	 */
+	void setTexture(int texture) {
+
+		m_texture = texture;
+	}
+
+	private String m_label;
+	private int m_texture = 0;
 }
