085b0bda6391e13d733a5ba14c0ddccf1f78ced9
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-31 21:47:23 +0200

    Implemented InputTextButton class

---
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/gui/InputTextButton.java
@@ -0,0 +1,79 @@
+/*
+
+Alessandro della Frattina 753073 VA
+Cristian Capiferri 752918 VA
+Francesco Lops 753175 VA
+Dariia Sniezhko 753057 VA
+
+*/
+
+package climatemonitoring.core.gui;
+
+import imgui.ImGui;
+
+/**
+ * Same as {@link InputText}, but with a button on the right (useful to implement a show/hide password)
+ * 
+ * @author adellafrattina
+ * @version 1.0-SNAPSHOT
+ */
+public class InputTextButton extends InputText {
+
+	/**
+	 * Initialize InputText fields
+	 * @param label The input text box label
+	 * @param str The string that will be rendered inside the box for the first time
+	 * @param error_msg The error message that will be shown when the {@link #showErrorMsg(boolean)} method will enable the error message rendering
+	 */
+	public InputTextButton(String label, String str, String error_msg) {
+
+		super(label, str, error_msg);
+	}
+
+	/**
+	 * Initialize InputText fields
+	 * @param label The input text box label
+	 * @param str The string that will be rendered inside the box for the first time
+	 */
+	public InputTextButton(String label, String str) {
+
+		super(label, str);
+	}
+
+	/**
+	 * Initialize InputText fields
+	 * @param label The input text box label
+	 */
+	public InputTextButton(String label) {
+
+		super(label);
+	}
+
+	/**
+	 * Renders to screen the input text box
+	 * @return True when the user is typing by default, but if the {@link #setEnterReturnsTrue(boolean)} method set the flag to true, then the function will return true only if the user presses enter while the input text has focus
+	 */
+	public boolean render() {
+
+		begin();
+		float width = m_width == DEFAULT_HEIGHT ? Math.abs(getWidth() - m_button.getWidth()) : getWidth();
+		ImGui.pushItemWidth(width);
+		ImGui.setCursorPos(getPositionX() - getOriginX(), getPositionY() - getOriginY());
+		boolean value = ImGui.inputText("##" + m_label, m_string, m_flags);
+		ImGui.popItemWidth();
+		value = end(value);
+		m_button.setPosition(getPositionX() - getOriginX() + width + 2.0f, SAME_LINE_Y);
+		m_button.setHeight(ImGui.getFrameHeight());
+		isButtonPressed = m_button.render();
+
+		return value;
+	}
+
+	public boolean isButtonPressed() {
+
+		return isButtonPressed;
+	}
+
+	private Button m_button = new Button("##");
+	private boolean isButtonPressed = false;
+}
