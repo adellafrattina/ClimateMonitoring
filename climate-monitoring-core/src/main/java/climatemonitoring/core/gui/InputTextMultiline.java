ff5c7afb4639c1393a0dcfd6836cbd42e4dea3a3
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-31 17:40:09 +0200

    Implemented InputTextMultiline class

--- climate-monitoring-core/src/main/java/climatemonitoring/core/gui/InputTextMultiline.java
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/gui/InputTextMultiline.java
@@ -9,7 +9,57 @@ Dariia Sniezhko 753057 VA
 
 package climatemonitoring.core.gui;
 
-public class InputTextMultiline extends Widget {
+import imgui.ImGui;
 
+/**
+ * Same as {@link InputText}, but on multiple lines
+ * 
+ * @author adellafrattina
+ * @version 1.0-SNAPSHOT
+ */
+public class InputTextMultiline extends InputText {
 
+	/**
+	 * Initialize InputText fields
+	 * @param label The input text box label
+	 * @param str The string that will be rendered inside the box for the first time
+	 * @param error_msg The error message that will be shown when the {@link #showErrorMsg(boolean)} method will enable the error message rendering
+	 */
+	public InputTextMultiline(String label, String str, String error_msg) {
+
+		super(label, str, error_msg);
+	}
+
+	/**
+	 * Initialize InputText fields
+	 * @param label The input text box label
+	 * @param str The string that will be rendered inside the box for the first time
+	 */
+	public InputTextMultiline(String label, String str) {
+
+		super(label, str);
+	}
+
+	/**
+	 * Initialize InputText fields
+	 * @param label The input text box label
+	 */
+	public InputTextMultiline(String label) {
+
+		super(label);
+	}
+
+	/**
+	 * Renders to screen the input text box
+	 * @return True when the user is typing by default, but if the {@link #setEnterReturnsTrue(boolean)} method set the flag to true, then the function will return true only if the user presses ctrl + enter while the input text has focus
+	 */
+	public boolean render() {
+
+		begin();
+		ImGui.setCursorPos(getPositionX() - getOriginX(), getPositionY() - getOriginY());
+		final boolean value = ImGui.inputTextMultiline("##" + m_label, m_string, getWidth(), getHeight(), m_flags);
+		end();
+
+		return value;
+	}
 }
