12d6cb6486e3706f3d98658b71bd695fda19c797
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-08-31 16:27:11 +0200

    Implemented Text class

--- climate-monitoring-core/src/main/java/climatemonitoring/core/gui/Text.java
+++ climate-monitoring-core/src/main/java/climatemonitoring/core/gui/Text.java
@@ -9,7 +9,61 @@ Dariia Sniezhko 753057 VA
 
 package climatemonitoring.core.gui;
 
-public class Text {
+import imgui.ImGui;
 
+/**
+ * To render some plain text
+ * 
+ * @author adellfrattina
+ * @version 1.0-SNAPSHOT
+ * @see Widget
+ */
+public class Text extends Widget {
 
+	/**
+	 * To create a text
+	 * @param str The text's string
+	 */
+	public Text(String str) {
+
+		m_str = str;
+	}
+
+	/**
+	 * To render the text
+	 */
+	public void render() {
+
+		ImGui.setCursorPos(getPositionX(), getPositionY());
+		//ImGui.pushItemWidth(getWidth());
+		ImGui.textColored(color[0], color[1], color[2], color[3], m_str);
+		//ImGui.popItemWidth();
+	}
+
+	/**
+	 * To set the text's string
+	 * @param str The text's string
+	 */
+	public void setString(String str) {
+
+		m_str = str;
+	}
+
+	/**
+	 * To set the text's color
+	 * @param r The red value (between 0 and 255)
+	 * @param g The green value (between 0 and 255)
+	 * @param b The blue value (between 0 and 255)
+	 * @param a The alpha value (between 0 and 255)
+	 */
+	public void setColor(int r, int g, int b, int a) {
+
+		color[0] = r;
+		color[1] = g;
+		color[2] = b;
+		color[3] = a;
+	}
+
+	private String m_str;
+	private final int[] color = { 255, 255, 255, 255 };
 }
