7ce18cbe44c70a9e185ca0ba2c185896505da6dd
adellafrattina <adellafrattina@studenti.uninsubria.it>
2024-09-05 22:21:35 +0200

    Implemented ParameterInfo view class

--- climate-monitoring-client/src/main/java/climatemonitoring/ParameterInfo.java
+++ climate-monitoring-client/src/main/java/climatemonitoring/ParameterInfo.java
@@ -11,11 +11,22 @@ package climatemonitoring;
 
 import java.util.StringTokenizer;
 
+import climatemonitoring.core.Application;
+import climatemonitoring.core.Area;
 import climatemonitoring.core.Category;
+import climatemonitoring.core.Center;
 import climatemonitoring.core.ConnectionLostException;
 import climatemonitoring.core.DatabaseRequestException;
 import climatemonitoring.core.Parameter;
+import climatemonitoring.core.Result;
+import climatemonitoring.core.gui.Button;
+import climatemonitoring.core.gui.Dropdown;
+import climatemonitoring.core.gui.Panel;
+import climatemonitoring.core.gui.Table;
+import climatemonitoring.core.gui.Text;
+import climatemonitoring.core.gui.Widget;
 import climatemonitoring.core.headless.Console;
+import imgui.ImGui;
 
 /**
  * To show all the parameters based on an area. This is not a typical view state, because it behaves differenty in GUI and Headless mode
@@ -115,9 +126,9 @@ public class ParameterInfo {
 
 			} while (errorMsg != null);
 
-			m_parameters = Handler.getProxyServer().getParameters(m_geonameID, m_centerID, m_category);
+			get().parameters = Handler.getProxyServer().getParameters(m_geonameID, m_centerID, m_category);
 
-			if (m_parameters == null || m_parameters.length == 0) {
+			if (get().parameters == null || get().parameters.length == 0) {
 
 				Console.write("No available parameters");
 				return;
@@ -126,11 +137,11 @@ public class ParameterInfo {
 			Console.write("--- " + Handler.getProxyServer().getArea(m_geonameID).getAsciiName() + " parameters ---");
 			Console.write("From " + m_centerID);
 			Console.write("Grouped by " + m_category);
-			Console.write("Count: " + m_parameters.length);
+			Console.write("Count: " + get().parameters.length);
 			Console.write("Average: " + Handler.getProxyServer().getParametersAverage(m_geonameID, m_centerID, m_category));
 
 			// Show parameters
-			for (Parameter parameter : m_parameters)
+			for (Parameter parameter : get().parameters)
 				Console.write("[" + parameter.getDate() + " " + parameter.getTime() + "] [by " + parameter.getUserID() + "] --- " + parameter.getScore() + " (" + parameter.getNotes() + ")");
 		}
 
@@ -153,11 +164,343 @@ public class ParameterInfo {
 
 	public static void onGUIRender() {
 
-		throw new UnsupportedOperationException("Unimplemented method 'onGUIRender'");
+		if (SearchArea.getSelectedArea() == null) {
+
+			Console.error("The selected area should not be null");
+			Handler.getView().returnToPreviousState();
+		}
+
+		else if (get().selectedArea != null) {
+
+			if (get().selectedArea.getGeonameID() != SearchArea.getSelectedArea().getGeonameID()) {
+
+				resetData();
+				return;
+			}
+		}
+
+		try {
+
+			get().render();
+		}
+
+		catch (ConnectionLostException e) {
+
+			Handler.getView().setCurrentState(ViewType.CONNECTION);
+		}
+
+		catch (Exception e) {
+
+			e.printStackTrace();
+			Application.close();
+		}
 	}
 
-	private static Parameter[] m_parameters;
+	private void render() throws ConnectionLostException, Exception {
+
+		if (get().loadInitialData) {
+
+			get().loadInitialData();
+			return;
+		}
+
+		else if (get().notMonitored) {
+
+			ImGui.text("NOTE: This area does not have any parameters / is not monitored by any center");
+			return;
+		}
+
+		int currentCenterIndex = get().selectCenter.render();
+		ImGui.sameLine();
+		ImGui.button("view");
+		int currentCategoryIndex = get().selectCategory.render();
+		ImGui.sameLine();
+		ImGui.text("(" + get().selectedCategory.getExplanation() + ")");
+
+		if (get().selectCenter.isAnyItemSelected() || get().selectCategory.isAnyItemSelected())
+			get().loadNewData = true;
+
+		if (get().loadNewData) {
+
+			get().loadNewData(currentCenterIndex, currentCategoryIndex);
+			get().loadingText.setOrigin(get().loadingText.getWidth() / 2.0f, get().loadingText.getHeight() / 2.0f);
+			get().loadingText.setPosition(ImGui.getWindowWidth() / 2.0f, ImGui.getWindowHeight() / 2.0f);
+			get().loadingText.render();
+			return;
+		}
+
+		if (dataNotFound) {
+
+			ImGui.textColored(255, 0.0f, 0.0f, 255, "No available data");
+			if (Handler.isOperatorLoggedIn()) {
+
+				if (Handler.getLoggedOperator().getCenterID().equals(selectedCenter.getCenterID())) {
+
+					ImGui.text("Record a new parameter");
+					add.setOriginX(0);
+					add.setPositionX(Widget.DEFAULT_X);
+					ImGui.sameLine();
+					if (add.render())
+						Handler.getView().setCurrentState(ViewType.PARAMETER_CREATION);
+				}
+			}
+
+			return;
+		}
+
+		if (parameters != null) {
+
+			// Show parameters
+			ImGui.text("Recordings count: " + parameters.length);
+			ImGui.text("Score average: " + average);
+			
+			tablePanelLabel.setOriginX(tablePanelLabel.getWidth() / 2.0f);
+			tablePanelLabel.setPositionX(tablePanel.getPositionX() + tablePanel.getWidth() / 2.0f);
+			tablePanelLabel.render();
+
+			if (Handler.isOperatorLoggedIn()) {
+
+				if (Handler.getLoggedOperator().getCenterID().equals(selectedCenter.getCenterID())) {
+
+					ImGui.sameLine();
+					add.setOriginX(add.getWidth());
+					add.setPositionX(tablePanel.getPositionX() + tablePanel.getWidth());
+					if (add.render()) {
+
+						Handler.getView().setCurrentState(ViewType.PARAMETER_CREATION);
+						return;
+					}
+				}
+			}
+
+			tablePanel.begin(null);
+			table.render();
+			tablePanel.end();
+		}
+	}
+
+	public static ParameterInfo get() {
+
+		return s_instance;
+	}
+
+	private void loadInitialData() throws ConnectionLostException, Exception {
+
+		if (requestInitialData) {
+
+			selectedArea = SearchArea.getSelectedArea();
+			associatedCentersResult = Handler.getProxyServerMT().getAssociatedCenters(selectedArea.getGeonameID());
+			latestCenterResult = Handler.getProxyServerMT().getLatestCenter(selectedArea.getGeonameID());
+			categoriesResult = Handler.getProxyServerMT().getCategories();
+
+			requestInitialData = false;
+		}
+
+		if (associatedCentersResult != null && associatedCentersResult.ready()) {
+
+			if (associatedCentersResult.get() == null || associatedCentersResult.get().length == 0) {
+
+				notMonitored = true;
+				loadInitialData = false;
+			}
+
+			else
+				centers = associatedCentersResult.get();
+
+			associatedCentersResult = null;
+		}
+
+		else if (associatedCentersResult == null) {
+
+			// Last center
+			if (latestCenterResult != null && latestCenterResult.ready()) {
+
+				String[] centerIDs = new String[centers.length];
+
+				int index = 0;
+				for (int i = 0; i < centerIDs.length; i++) {
+
+					centerIDs[i] = centers[i].getCenterID();
+					if (centerIDs[i].equals(latestCenterResult.get().getCenterID()))
+						index = i;
+				}
+
+				selectCenter = new Dropdown("Select center:	  ", centerIDs);
+				selectCenter.setCurrentItem(index);
+				selectedCenter = centers[index];
+
+				latestCategoryResult = Handler.getProxyServerMT().getLatestCategory(selectedArea.getGeonameID(), latestCenterResult.get().getCenterID());
+				latestCenterResult = null;
+			}
+
+			else if (latestCenterResult == null) {
+
+				// Last category
+				if (categoriesResult != null && categoriesResult.ready() && latestCategoryResult != null && latestCategoryResult.ready()) {
+
+					Category[] categories = categoriesResult.get();
+					String[] categoriesName = new String[categories.length];
+
+					int index = 0;
+					for (int i = 0; i < categoriesName.length; i++) {
+		
+						categoriesName[i] = categories[i].getCategory();
+						if (categoriesName[i].equals(latestCategoryResult.get().getCategory()))
+							index = i;
+					}
+
+					selectCategory = new Dropdown("Select category: ", categoriesName);
+					selectCategory.setCurrentItem(index);
+					selectedCategory = categories[index];
+
+					parametersResult = Handler.getProxyServerMT().getParameters(selectedArea.getGeonameID(), selectedCenter.getCenterID(), selectedCategory.getCategory());
+					latestCategoryResult = null;
+					categoriesResult = null;
+				}
+
+				else if (categoriesResult == null && latestCategoryResult == null) {
+
+					// Parameters
+					if (parametersResult != null && parametersResult.ready()) {
+
+						parameters = parametersResult.get();
+						if (parameters != null && parameters.length != 0)
+							averageResult = Handler.getProxyServerMT().getParametersAverage(selectedArea.getGeonameID(), selectedCenter.getCenterID(), selectedCategory.getCategory());
+
+						parametersResult = null;
+					}
+
+					else if (parametersResult == null) {
+
+						// Parameters average
+						if (averageResult != null && averageResult.ready()) {
+
+							average = averageResult.get();
+							setUpTable();
+							averageResult = null;
+						}
+
+						else if (averageResult == null) {
+
+							loadInitialData = false;
+							requestInitialData = true;
+						}
+					}
+				}
+			}
+		}
+
+		loadingText.setOrigin(loadingText.getWidth() / 2.0f, loadingText.getHeight() / 2.0f);
+		loadingText.setPosition(ImGui.getWindowWidth() / 2.0f, ImGui.getWindowHeight() / 2.0f);
+		loadingText.render();
+	}
+
+	private void loadNewData(int current_center_index, int current_category_index) throws ConnectionLostException, Exception {
+
+		if (requestNewData) {
+
+			currentCenterResult = Handler.getProxyServerMT().getCenter(selectCenter.getList()[current_center_index]);
+			categoriesResult = Handler.getProxyServerMT().getCategories();
+			parametersResult = Handler.getProxyServerMT().getParameters(selectedArea.getGeonameID(), selectCenter.getList()[current_center_index], selectCategory.getList()[current_category_index]);
+			requestNewData = false;
+		}
+
+		if (currentCenterResult != null && currentCenterResult.ready() && categoriesResult != null && categoriesResult.ready() && parametersResult != null && parametersResult.ready()) {
+
+			// Center
+			selectedCenter = currentCenterResult.get();
+
+			// Category
+			Category[] categories = categoriesResult.get();
+			selectedCategory = categories[current_category_index];
+
+			// Parameters
+			parameters = parametersResult.get();
+			if (parameters != null && parameters.length != 0) {
+
+				dataNotFound = false;
+				averageResult = Handler.getProxyServerMT().getParametersAverage(selectedArea.getGeonameID(), selectedCenter.getCenterID(), selectedCategory.getCategory());
+			}
+
+			else
+				dataNotFound = true;
+
+			currentCenterResult = null;
+			categoriesResult = null;
+			parametersResult = null;
+		}
+
+		else if (currentCenterResult == null && categoriesResult == null && parametersResult == null) {
+
+			if (dataNotFound) {
+
+				loadNewData = false;
+				requestNewData = true;
+				averageResult = null;
+				return;
+			}
+
+			if (averageResult != null && averageResult.ready()) {
+
+				// Parameters average
+				average = averageResult.get();
+				setUpTable();
+				loadNewData = false;
+				requestNewData = true;
+				averageResult = null;
+			}
+		}
+	}
+
+	private void setUpTable() {
+
+		table = new Table("Parameters", new String[] { "Date", "Time", "Operator", "Score", "Notes" });
+
+		for (Parameter parameter : parameters)
+			table.addRow(new String[] { parameter.getDate(), parameter.getTime(), parameter.getUserID(), "" + parameter.getScore(), parameter.getNotes() });
+
+		for (int i = 0; i < 20; i++)
+			table.addRow(new String[] { "parameter.getDate()", "parameter.getTime()", "parameter.getUserID()", "" + "parameter.getScore()", "parameter.getNotes()" });
+	}
+
+	public static void resetData() {
+
+		s_instance = new ParameterInfo();
+	}
+
+	private static ParameterInfo s_instance = new ParameterInfo();
+
+	private Parameter[] parameters;
+
+	// Headless
 	private static int m_geonameID;
 	private static String m_centerID;
 	private static String m_category;
+
+	// GUI
+	private boolean notMonitored = false;
+	private boolean dataNotFound = false;
+	private boolean requestInitialData = true;
+	private boolean loadInitialData = true;
+	private boolean requestNewData = true;
+	private boolean loadNewData = false;
+	private Text loadingText = new Text("Loading...");
+	private Area selectedArea;
+	private Dropdown selectCenter;
+	private Dropdown selectCategory;
+	private Result<Center[]> associatedCentersResult;
+	private Result<Center> latestCenterResult;
+	private Result<Center> currentCenterResult;
+	private Center[] centers;
+	private Center selectedCenter;
+	private Result<Category[]> categoriesResult;
+	private Result<Category> latestCategoryResult;
+	private Category selectedCategory;
+	private Result<Double> averageResult;
+	private double average;
+	private Result<Parameter[]> parametersResult;
+	private Table table;
+	private Panel tablePanel = new Panel();
+	private Text tablePanelLabel = new Text("Recordings");
+	private Button add = new Button(" + ");
 }
