86b76d94f3d15046fe5290a5f14052cf169c3336
ccapiferri <ccapiferri@studenti.uninsubria.it>
2024-09-08 17:33:52 +0200

    Added base layout for CenterCreation

--- climate-monitoring-client/src/main/java/climatemonitoring/CenterCreation.java
+++ climate-monitoring-client/src/main/java/climatemonitoring/CenterCreation.java
@@ -9,11 +9,17 @@ Dariia Sniezhko 753057 VA
 
 package climatemonitoring;
 
+import climatemonitoring.core.Application;
 import climatemonitoring.core.Center;
 import climatemonitoring.core.ConnectionLostException;
 import climatemonitoring.core.DatabaseRequestException;
 import climatemonitoring.core.ViewState;
+import climatemonitoring.core.gui.Button;
+import climatemonitoring.core.gui.InputText;
+import climatemonitoring.core.gui.Panel;
+import climatemonitoring.core.gui.Text;
 import climatemonitoring.core.headless.Console;
+import imgui.ImGui;
 
 /**
  * To add a new center
@@ -107,7 +113,87 @@ class CenterCreation extends ViewState {
 	@Override
 	public void onGUIRender() {
 
-		throw new UnsupportedOperationException("Unimplemented method 'onGUIRender'");
+		m_panel.setSize(Application.getWidth() / 2.0f, m_cancelButton.getPositionY() - ImGui.getCursorPosY() - 50);
+		m_panel.setOriginX(m_panel.getWidth() / 2.0f);
+		m_panel.setPositionX(Application.getWidth() / 2.0f);
+
+		m_panel.begin("Center Creation");
+
+		ImGui.newLine();
+
+		m_centerIDInputText.setWidth(m_panel.getWidth() / 2.0f);
+		m_centerIDInputText.setOriginX(m_centerIDInputText.getWidth() / 2.0f);
+		m_centerIDInputText.setPositionX(m_panel.getWidth() / 2.0f);
+		m_centerIDInputText.render();
+
+		ImGui.newLine();
+
+		m_streetInputText.setWidth(m_panel.getWidth() / 2.0f);
+		m_streetInputText.setOriginX(m_streetInputText.getWidth() / 2.0f);
+		m_streetInputText.setPositionX(m_panel.getWidth() / 2.0f);
+		m_streetInputText.render();
+
+		ImGui.newLine();
+
+		m_houseNumberInputText.setWidth(m_panel.getWidth() / 2.0f);
+		m_houseNumberInputText.setOriginX(m_houseNumberInputText.getWidth() / 2.0f);
+		m_houseNumberInputText.setPositionX(m_panel.getWidth() / 2.0f);
+		m_houseNumberInputText.render();
+
+		ImGui.newLine();
+
+		m_postalCodeInputText.setWidth(m_panel.getWidth() / 2.0f);
+		m_postalCodeInputText.setOriginX(m_postalCodeInputText.getWidth() / 2.0f);
+		m_postalCodeInputText.setPositionX(m_panel.getWidth() / 2.0f);
+		m_postalCodeInputText.render();
+
+		ImGui.newLine();
+
+		m_cityText.setWidth(m_panel.getWidth() / 2.0f);
+		m_cityText.setOriginX(m_cityText.getWidth() / 2.0f);
+		m_cityText.setPositionX(m_panel.getWidth() / 2.0f);
+		m_cityText.render();
+
+		if (!m_showSearchBox && !m_citySelection.getString().isEmpty()) {
+
+			m_citySelection.setWidth(ImGui.calcTextSizeX(m_citySelection.getString()) + 8.0f);
+			m_citySelection.setOriginX(m_citySelection.getWidth() / 2.0f);
+			m_citySelection.setPositionX(m_panel.getWidth() / 2.0f);
+			m_citySelection.setReadOnly(true);
+			m_citySelection.render();
+		}
+
+		m_showCityButton.setOriginX(m_showCityButton.getWidth() / 2.0f);
+		m_showCityButton.setPositionX(m_panel.getWidth() / 2.0f);
+		if (!m_showSearchBox && m_showCityButton.render())
+			m_showSearchBox = true;
+
+		else if (m_showSearchBox) {
+
+			m_searchArea.onGUIRender();
+
+			if (m_searchArea.isAnyAreaSelected()) {
+
+				m_showSearchBox = false;
+				m_citySelection.setString(m_searchArea.getSelectedArea().getAsciiName() + ", " + 
+											m_searchArea.getSelectedArea().getCountryCode());
+			}
+		}
+
+
+		ImGui.newLine();
+
+		m_districtInputText.setWidth(m_panel.getWidth() / 2.0f);
+		m_districtInputText.setOriginX(m_districtInputText.getWidth() / 2.0f);
+		m_districtInputText.setPositionX(m_panel.getWidth() / 2.0f);
+		m_districtInputText.render();
+
+		m_panel.end();
+
+		m_cancelButton.setOriginX(m_cancelButton.getWidth() / 2.0f);
+		m_cancelButton.setPositionX(m_panel.getPositionX());
+		if (m_cancelButton.render())
+			returnToPreviousState();
 	}
 
 	Center newCenter;
@@ -118,4 +204,21 @@ class CenterCreation extends ViewState {
 	private int m_postalCode;
 	private int m_city;
 	private String m_district;
+
+	private Panel m_panel = new Panel();
+	private InputText m_centerIDInputText = new InputText("Center ID");
+	private InputText m_streetInputText = new InputText("Street");
+	private InputText m_houseNumberInputText = new InputText("House number");
+	private InputText m_postalCodeInputText = new InputText("Postal code");
+	private InputText m_districtInputText = new InputText("District");
+
+	private boolean m_showSearchBox = false;
+	private Text m_cityText = new Text("City");
+	private Button m_showCityButton = new Button(" Select ");
+	private InputText m_citySelection = new InputText("");
+
+	private Button m_cancelButton = new Button(" Cancel ");
+	private Button m_createCenterButton = new Button(" Create center ");
+
+	private SearchArea m_searchArea = new SearchArea();
 }
